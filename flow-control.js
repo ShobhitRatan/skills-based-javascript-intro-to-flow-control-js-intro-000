e1d089f6162e2bcf774d4a6b75597ca1ad9f3f23
Shobhit Ratan <shobhit.ratan@baruchmail.cuny.edu>
2020-02-16 22:24:06 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -14,7 +14,12 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-
+  if (age > 12 && age < 20) {
+    return 'You are a teenager!';
+  }
+  else if (age < 13) {
+    return 'You are a kid';
+  }
 }
 
 function ternaryTeenager(age) {
